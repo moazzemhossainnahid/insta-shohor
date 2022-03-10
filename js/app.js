aad3d02f63e2bac81d01e2061321047546e20cab
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2022-03-11 00:10:25 +0600

    fix love react section

--- js/app.js
+++ js/app.js
@@ -154,6 +154,7 @@ const showPosts = (posts) => {
 };
 
 const displayLikedPosts = () => {
+  document.getElementById( "liked" ).innerHTML = '';
     const likedPosts = getLikedPosts();
     likedPosts.forEach((post) => {
         const div = createPost(post);
